ab00981bb7234972354438a6eb15df1554a5113d
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-02-22 18:16:08 +0200

    remove embeded isValidArgs tests

--- test/Array_Utils_Tests/filter.test.js
+++ test/Array_Utils_Tests/filter.test.js
@@ -11,19 +11,3 @@ test("returned array should have one element that is equal to the string 'osid' 
     const output = ["osid"];
     expect(filter(testArray,value => value === 'osid')).toEqual(output);
 })
-
-test("throw error if first argument is not an array",()=>{
-    expect(() => filter({},()=>1)).toThrow(Error);
-    expect(() => filter({a:1,b:2},()=>1)).toThrow(Error);
-    expect(() => filter(1,()=>1)).toThrow(Error);
-    expect(() => filter("wow",()=>1)).toThrow(Error);
-    expect(() => filter(null,()=>1)).toThrow(Error);    
-    expect(() => filter(undefined,()=>1)).toThrow(Error);
-})
-
-test("throw error if second argument is not a function",()=>{
-    expect(() => filter([1,2])).toThrow(Error);    
-    expect(() => filter([1,2],true)).toThrow(Error);
-    expect(() => filter([1,2],"nice")).toThrow(Error);
-    expect(() => filter([1,2],123)).toThrow(Error);
-})
--- test/Array_Utils_Tests/map.test.js
+++ test/Array_Utils_Tests/map.test.js
@@ -9,19 +9,3 @@ test("returned array has all strings in it uppercased",()=>{
     const testArray = ["osid",'is',"super","smart"];
     expect(map(testArray,value=>value.toUpperCase())).toEqual(["OSID",'IS',"SUPER","SMART"]);
 })
-
-test("throw error if first argument is not an array",()=>{
-    expect(() => map({},()=>1)).toThrow(Error);
-    expect(() => map({a:1,b:2},()=>1)).toThrow(Error);
-    expect(() => map(1,()=>1)).toThrow(Error);
-    expect(() => map("wow",()=>1)).toThrow(Error);
-    expect(() => map(null,()=>1)).toThrow(Error);    
-    expect(() => map(undefined,()=>1)).toThrow(Error);
-})
-
-test("throw error if second argument is not a function",()=>{
-    expect(() => map([1,2])).toThrow(Error);    
-    expect(() => map([1,2],true)).toThrow(Error);
-    expect(() => map([1,2],"nice")).toThrow(Error);
-    expect(() => map([1,2],123)).toThrow(Error);
-})
